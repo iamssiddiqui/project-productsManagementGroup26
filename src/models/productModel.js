683725f8545f018d967b7803d7153f769ee94823
imssiddiqui <106057264+imssiddiqui@users.noreply.github.com>
2022-07-27 11:29:15 +0530

    productmodel

---
+++ src/models/productModel.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose")
+
+const productModel = new mongoose.Schema (
+    {
+     title: {
+        type: String, 
+        required: true, 
+        unique: true,
+        trim: true
+    },
+
+    description: {
+        type: String, 
+        required: true,
+        trim: true
+    },
+
+    price: {
+        type: Number, 
+        required: true,
+        trim: true
+    },
+
+    currencyId: {
+        type: String, 
+        required: true,
+        default: "INR",
+        trim: true
+    },
+
+    currencyFormat: {
+        type: String, 
+        required: true,
+        default: "₹",
+        trim: true
+    },
+
+    isFreeShipping: {
+        type: boolean, 
+        default: false
+    },
+
+    productImage: {
+        type: String, 
+        required: true
+    },  // s3 link
+
+    style: {
+        type: String,
+        trim: true
+    },
+
+    availableSizes: {type: String, 
+        required: true,
+        minLength: 1,
+        trim: true,
+        enum: ["S", "XS","M","X", "L","XXL", "XL"]
+    },
+
+    installments: {
+        type: Number,
+        trim: true
+    },
+
+    deletedAt: {
+        type: Date
+    }, 
+
+    isDeleted: {
+        type: Boolean, 
+        default: false
+    },
+},{timestamps: true});
+
+module.exports = mongoose.model('Product', productModel)
